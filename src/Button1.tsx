fc985f825fdc851c9540d617f94def90066bcf26
COLLEGELAB-09 <COLLEGELAB-09@BJSB.LAB>
2026-05-09 16:55:20 +0800

    Initial Commit

---
+++ src/Button1.tsx
@@ -0,0 +1,14 @@
+
+export default function Button1() {
+    // const handleClick = () => {
+    //     alert("Button 1 Clicked");
+    // }; 
+    // Suggested, much cleaner look
+
+    return (
+        // <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => alert ("Button 1 clicked")}>
+            Click Me
+            </button>
+    )
+}
